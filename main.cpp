95c27f21405a4604ad8267493109a6330ec08421
Benjaminas1 <31960012+Benjaminas1@users.noreply.github.com>
2021-09-20 19:00:41 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+void initialiseHash(char *hashed){
+    
+}
+
+void printHash(char *hashed){
+    
+}
+
+int sumIntElements(int number){
+    
+}
+
+void hashAWord(string &word, char *hashed){
+    
+}
+
+int main(){
+
+    
+}
